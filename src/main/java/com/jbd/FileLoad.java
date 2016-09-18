7039f1a1ffa8aa5613c3fd412bb87fd3b0719a05
Michał Piechota <mchpiechota@gmail.com>
2016-09-18 20:17:30 +0200

    revert : dodanie nowej klasy FileLoad oraz troche mięsa w środku

--- src/main/java/com/jbd/FileLoad.java
+++ src/main/java/com/jbd/FileLoad.java
@@ -1,33 +1,36 @@
 package com.jbd;
-
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Path;
 
 public class FileLoad {
 
-    FileReader filer = null;
-    String line = "";
-    BufferedReader bfr = new BufferedReader(filer);
-
-    public void fileLoad(Path inputPath) {
+    void metoda(){
+        FileReader filer = null;
+        String linia = "";
         try {
-            filer = new FileReader(String.valueOf(inputPath));
+            filer = new FileReader("nazwa pliku");
         } catch (FileNotFoundException e) {
             System.out.println("BŁĄD PRZY OTWIERANIU PLIKU!");
             System.exit(1);
         }
+
+        BufferedReader bfr = new BufferedReader(filer);
     }
-    public void fileRead() {
-        try {
-            while ((line = bfr.readLine()) != null) {
-                System.out.println(line);
-            }
-        } catch (IOException e) {
-            System.out.println("BŁĄD ODCZYTU Z PLIKU!");
-            System.exit(2);
-        }
+
+
+    public static void main(String[] inputPath) {
+
+
+
+
+
     }
+
+
+
+
+
+
 }
