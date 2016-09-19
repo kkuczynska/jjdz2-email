65f6c2141af8105825448d32d434348c032d5528
Karolina Kuczyńska <kaarolcia.k@gmail.com>
2016-09-19 21:13:00 +0200

    now start date is validated

--- src/main/java/com/jbd/SearchCriteriaValidator.java
+++ src/main/java/com/jbd/SearchCriteriaValidator.java
@@ -1,6 +1,12 @@
 package com.jbd;
 
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import java.util.Calendar;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SearchCriteriaValidator {
 
     public static void validateEmail(String email) {
@@ -20,8 +26,9 @@ public class SearchCriteriaValidator {
         }
     }
 
-
-
+    public static void validateStartDate(String startDate) {
+        validateDate(startDate);
+    }
 
     private static int validateUnacceptableCharsExisting (String validateInput, String[] unacceptableChars) {
         int validationBinary = 0;
@@ -33,12 +40,46 @@ public class SearchCriteriaValidator {
         }
 
         return validationBinary;
-
     }
 
+    private static void validateDate(String date) {
+        int validateBinary = 0;
 
+        String datePattern = "([0-3][0-9])/([01][0-9])/([12][09][0-9][0-9])";
 
+        Pattern compile = Pattern.compile(datePattern);
+        Matcher matcher = compile.matcher(date);
 
+        boolean matches = Pattern.matches(datePattern, date);
+        if(matches==false) {
+            validateBinary = 1;
+        }
 
+        while (matcher.find()) {
+            int day = Integer.parseInt(matcher.group(1));
+            int month = Integer.parseInt(matcher.group(2));
+            int year = Integer.parseInt(matcher.group(3));
+
+            validateBinary = validateDayMonthYear(day, month, year);
+
+        }
+
+        if(validateBinary==1) {
+            System.out.println("Błędna data.");
+        }
+    }
+
+    private static int validateDayMonthYear(int day, int month, int year) {
+        int validationBinary = 0;
+
+        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+
+        if(day > 31 || day == 0 ||
+                month > 12 || month == 0) {
+            validationBinary = 1;
+        }
+
+        return validationBinary;
+    }
 
 }
