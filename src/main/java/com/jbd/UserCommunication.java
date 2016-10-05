66fb819517ab2331dd3085c4c00868eb624024c8
Karolina Kuczyńska <kaarolcia.k@gmail.com>
2016-10-05 21:52:19 +0200

    now user response is separated in UserCommunication

--- src/main/java/com/jbd/UserCommunication.java
+++ src/main/java/com/jbd/UserCommunication.java
@@ -1,9 +1,15 @@
 package com.jbd;
 
+import java.util.Scanner;
+
 public class UserCommunication {
 
-    public static void sendUserMessage(String message) {
+    public void sendUserMessage(String message) {
         System.out.println(message);
     }
 
+    public String getUserResonse() {
+        Scanner userResponse = new Scanner(System.in);
+        return userResponse.nextLine();
+    }
 }
