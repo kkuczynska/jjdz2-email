59c85f1223c7f53af8164ff6e97f0b0decc777b2
Karolina Kuczyńska <kaarolcia.k@gmail.com>
2016-10-10 18:57:31 +0200

    refactor date validation

--- src/main/java/com/jbd/SearchCriteriaValidator.java
+++ src/main/java/com/jbd/SearchCriteriaValidator.java
@@ -1,8 +1,9 @@
 package com.jbd;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.regex.Pattern;
 
 public class SearchCriteriaValidator {
@@ -13,74 +14,47 @@ public class SearchCriteriaValidator {
     public static final int MAX_DAYS_IN_MONTH = 31;
     public static final int MAX_MONTHS_IN_YEAR = 12;
 
-    private static final String datePattern = "([0-3][0-9])/([01][0-9])/([12][09][0-9][0-9])";
-    public static Pattern compile = Pattern.compile(datePattern);
-
-    private static String getStartDate = SearchCriteria.getSTARTDATE();
+    private static final String DATE_PATTERN = "([0-3][0-9])/([01][0-9])/([12][09][0-9][0-9])";
 
     private static UserCommunication userMessage = new UserCommunication();
+    private static DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-    public boolean validateEmail(String email) {
+    public static boolean validateEmail(String email) {
+        String[] unacceptableChars = {"!", "#", "$", "%", "^", "&", "*", "(", ")", "+",
+                "=", "[", "]", "{", "}", "~", "`", "\\", "|", ":", ";", "\"", "'", ",", "<", ">", "?", "/"};
+
         boolean validationFlag = true;
+        boolean validateChars = validateUnacceptableCharsExisting(email,unacceptableChars);
 
-        if(!email.equals("")) {
-            String[] unacceptableChars = {"!", "#", "$", "%", "^", "&", "*", "(", ")", "+",
-                    "=", "[", "]", "{", "}", "~", "`", "\\", "|", ":", ";", "\"", "'", ",", "<", ">", "?", "/"};
-
-            boolean validateChars = validateUnacceptableCharsExisting(email, unacceptableChars);
-
-            if (validateChars == false || !email.contains("@") || !email.contains(".")) {
-                validationFlag = false;
-            }
+        if(validateChars==false || !email.contains("@") || !email.contains(".")) {
+            validationFlag = false;
         }
-
         if(validationFlag==false) {
             userMessage.sendUserMessage(INCORRECT_EMAIL_MESSAGE);
         }
         return validationFlag;
     }
 
-    public boolean validateStartDate(String startDate) {
-        boolean validationFlag = datePatternMatching(startDate);
+    public static boolean validateStartDate(String startDate) {
+        boolean validationFlag = datePatternMatching(startDate.toString());
         if(validationFlag==false) {
             userMessage.sendUserMessage(INCORRECT_DATE_MESSAGE);
         }
         return validationFlag;
     }
 
-    public boolean validateEndDate(String endDate) {
+    public static void validateEndDate(String endDate) {
         boolean validationFlag = datePatternMatching(endDate);
 
-        if(validationFlag==true) {
-            SearchCriteriaValidator dateGrouping = new SearchCriteriaValidator();
-
-            List<Integer> endDateList = dateGrouping.datePatternGrouping(endDate);
-            List<Integer> startDateList = dateGrouping.datePatternGrouping(getStartDate);
-
-            long startYear = dateGrouping.datePatternGroupingYear(getStartDate);
-            long endYear = dateGrouping.datePatternGroupingYear(endDate);
-
-            int startMonth = startDateList.get(1);
-            int endMonth = endDateList.get(1);
-            int startDay = startDateList.get(0);
-            int endDay = endDateList.get(0);
-
-            if (startYear > endYear) {
-                validationFlag = false;
-            } else if (startYear == endYear
-                    && startMonth > endMonth) {
-                validationFlag = false;
-            } else if (startYear == endYear
-                    && startMonth == endMonth
-                    && startDay > endDay) {
-                validationFlag = false;
-            }
+        LocalDate endDateFormatted = null;
+        boolean isEndDateAfterStartDate = endDateFormatted.isAfter(LocalDate.parse(SearchCriteria.getSTARTDATE(), dateFormatter));
+        if(isEndDateAfterStartDate==false) {
+            validationFlag = false;
         }
 
         if (validationFlag == false) {
             userMessage.sendUserMessage(INCORRECT_DATE_MESSAGE);
         }
-        return validationFlag;
     }
 
     private static boolean validateUnacceptableCharsExisting (String validateInput, String[] unacceptableChars) {
@@ -96,48 +70,16 @@ public class SearchCriteriaValidator {
     }
 
     private static boolean datePatternMatching(String date) {
-        boolean validationFlag = true;
+        boolean validationFlag;
 
-        SearchCriteriaValidator dateGrouping = new SearchCriteriaValidator();
-        Matcher matcher = compile.matcher(date);
-
-        boolean matches = Pattern.matches(datePattern, date);
+        boolean matches = Pattern.matches(DATE_PATTERN, date);
         if(matches==false) {
             validationFlag = false;
         } else {
-            List<Integer> dateList = dateGrouping.datePatternGrouping(date);
-            long year = dateGrouping.datePatternGroupingYear(date);
-            validationFlag = validateDayMonthYear(dateList.get(0), dateList.get(1), year);
+            LocalDate endDateFormatted = LocalDate.parse(date, dateFormatter);
+            validationFlag = validateDayMonthYear(endDateFormatted.getDayOfMonth(), endDateFormatted.getMonthValue(), endDateFormatted.getYear());
         }
         return validationFlag;
-    }
-
-    private List<Integer> datePatternGrouping(String date) {
-        List<Integer> dateGrouped = new ArrayList<>();
-
-        Matcher matcher = compile.matcher(date);
-
-        while (matcher.find()) {
-            int day = Integer.parseInt(matcher.group(1));
-            int month = Integer.parseInt(matcher.group(2));
-
-            dateGrouped.add(day);
-            dateGrouped.add(month);
-        }
-
-        return dateGrouped;
-    }
-
-    private long datePatternGroupingYear(String date) {
-        long dateYear = 0000;
-
-        Matcher matcher = compile.matcher(date);
-
-        while (matcher.find()) {
-            dateYear = Integer.parseInt(matcher.group(3));
-        }
-
-        return dateYear;
     }
 
     private static boolean validateDayMonthYear(int day, int month, long year) {
