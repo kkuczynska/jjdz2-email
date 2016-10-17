c7bd13f2ca08ac0c79474ab2143834d11edd5bbe
Cinek1812 <marbar1812@gmail.com>
2016-10-17 13:06:39 +0200

    Add new function DisplayEmails

---
+++ src/main/java/com/jbd/DisplayEmails.java
@@ -0,0 +1,62 @@
+package com.jbd;
+
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Created by Admin on 15.10.2016.
+ */
+public class DisplayEmails {
+    public final int WHOLE_EMAIL = 1;
+    public final int JUST_EMAIL_ADDRESS = 2;
+
+    public void DisplayEmails(List<Email> emailList) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int wybor = 0;
+        boolean flaga = false;
+
+
+        do {
+            System.out.println("What would you like to display ?");
+            System.out.println("1 - Whole e-mail");
+            System.out.println("2 - Only e-mail's addresses");
+            wybor = scanner.nextInt();
+            if (wybor == 1 || wybor == 2) {
+                flaga = true;
+            } else {
+                flaga = false;
+                System.out.println("Wrong Option! Choose correctly.");
+            }
+
+
+        } while (!flaga);
+
+
+        switch (wybor) {
+            case WHOLE_EMAIL:
+                int counter = 0;
+                System.out.println("You have taken option 1- whole email");
+                for (Email e : emailList) {
+                    System.out.print(counter + ". ");
+                    System.out.println(e);
+                    System.out.println(e.getContent());
+                    System.out.println("-----------------------------------------");
+                    counter++;
+                }
+                break;
+            case JUST_EMAIL_ADDRESS:
+                System.out.println("You have taken option 2 - just email address");
+                counter = 0;
+                for (Email e : emailList) {
+                    System.out.println(counter + ". > " + e.getFrom());
+                    counter++;
+                }
+                break;
+
+        }
+
+    }
+}
+
