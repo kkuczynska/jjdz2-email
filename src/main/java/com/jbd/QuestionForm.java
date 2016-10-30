35b084f10a8a42c5180f8317a88b69e9ab978f8d
Karolina Kuczyńska <kaarolcia.k@gmail.com>
2016-10-30 20:38:35 +0100

    refactor of class in order to add a keywordsForm method

--- src/main/java/com/jbd/QuestionForm.java
+++ src/main/java/com/jbd/QuestionForm.java
@@ -13,36 +13,48 @@ public class QuestionForm {
     private static String STARTDATE_QUESTION_WRONG_VALUE = "Wrong format or invalid date, please provide new start date (YYYY-MM-DD)";
     private static String ENDDATE_QUESTION_WRONG_VALUE = "Wrong format or invalid date, please provide new end date (YYYY-MM-DD)";
 
-    public void askQuestionsAndSetAnswers() {
-        UserCommunication sendMessage = new UserCommunication();
+    private UserCommunication userCommunication = new UserCommunication();
+    private static final String KEYWORDS_HELP_QUESTION = "Do you need help with keywords? (yes / no)";
+
+    public void searchCriteriaForm() {
+
         SearchCriteriaValidator searchCriteriaValidator = new SearchCriteriaValidator();
         SearchCriteria searchCriteria = new SearchCriteria();
 
         do {
-            sendMessage.sendUserMessage(EMAIL_QUESTION);
-            setEMAIL(sendMessage.getUserResponse());
+            userCommunication.sendUserMessage(EMAIL_QUESTION);
+            setEMAIL(userCommunication.getUserResponse());
             if(searchCriteriaValidator.validateEmail(searchCriteria.getEMAIL())==false) {
                 EMAIL_QUESTION=EMAIL_QUESTION_WRONG_VALUE;
             }
         } while(!searchCriteriaValidator.validateEmail(searchCriteria.getEMAIL()));
 
         do {
-        sendMessage.sendUserMessage(STARTDATE_QUESTION);
-        setSTARTDATE(sendMessage.getUserResponse());
+        userCommunication.sendUserMessage(STARTDATE_QUESTION);
+        setSTARTDATE(userCommunication.getUserResponse());
             if(searchCriteriaValidator.validateStartDate(searchCriteria.getSTARTDATE())==false) {
                 STARTDATE_QUESTION=STARTDATE_QUESTION_WRONG_VALUE;
             }
         } while(!searchCriteriaValidator.validateStartDate(searchCriteria.getSTARTDATE()));
 
         do {
-        sendMessage.sendUserMessage(ENDDATE_QUESTION);
-        setENDDATE(sendMessage.getUserResponse());
+        userCommunication.sendUserMessage(ENDDATE_QUESTION);
+        setENDDATE(userCommunication.getUserResponse());
             if(searchCriteriaValidator.validateEndDate(searchCriteria.getENDDATE())==false) {
                 ENDDATE_QUESTION=ENDDATE_QUESTION_WRONG_VALUE;
             }
         } while(!searchCriteriaValidator.validateEndDate(searchCriteria.getENDDATE()));
 
-        sendMessage.sendUserMessage(KEYWORD_QUESTION);
-        setKEYWORDS(sendMessage.getUserResponse());
+        userCommunication.sendUserMessage(KEYWORD_QUESTION);
+        setKEYWORDS(userCommunication.getUserResponse());
+    }
+
+    public void keywordsForm() {
+        userCommunication.sendUserMessage(KEYWORDS_HELP_QUESTION);
+        String answer = userCommunication.getUserResponse();
+
+        if(answer.equals("yes")) {
+
+        }
     }
 }
