36b663b9fc615828e90613bff9c1c14ca85503c9
Karolina Kuczyńska <kaarolcia.k@gmail.com>
2016-10-30 18:29:59 +0100

    refactor of tests after refactor of code

--- src/test/java/com/jbd/SearchCriteriaValidatorTest.java
+++ src/test/java/com/jbd/SearchCriteriaValidatorTest.java
@@ -5,146 +5,116 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class SearchCriteriaValidatorTest {
-    private static SearchCriteriaValidator searchCriteria = new SearchCriteriaValidator();
-
-
-
+    private static SearchCriteriaValidator searchCriteriaValidator = new SearchCriteriaValidator();
+    private static SearchCriteria searchCriteria = new SearchCriteria();
 
     @Test
     public void empty_email_should_return_false() throws Exception {
         String emptyEmail = "";
-        assertEquals(false, searchCriteria.validateEmail(emptyEmail));
+        assertEquals(false, searchCriteriaValidator.validateEmail(emptyEmail));
     }
 
     @Test
     public void email_with_unacceptable_characters_should_return_false() throws Exception {
         String emailWithUnacceptableCharacters = "email#$%(*address@wp.pl";
-        assertEquals(false, searchCriteria.validateEmail(emailWithUnacceptableCharacters));
+        assertEquals(false, searchCriteriaValidator.validateEmail(emailWithUnacceptableCharacters));
     }
 
     @Test
     public void email_without_dot_should_return_false() throws Exception {
         String emailWithoutDot = "adressemail@wp";
-        assertEquals(false, searchCriteria.validateEmail(emailWithoutDot));
+        assertEquals(false, searchCriteriaValidator.validateEmail(emailWithoutDot));
     }
 
     @Test
     public void email_without_at_should_return_false() {
         String emailWithoutAt = "emailaddress.com";
-        assertEquals(false, searchCriteria.validateEmail(emailWithoutAt));
+        assertEquals(false, searchCriteriaValidator.validateEmail(emailWithoutAt));
     }
 
 
     @Test
     public void exceeded_day_in_startdate_should_return_false() {
-        String exceededDayInStartDate = "56/12/2016";
-        assertEquals(false, searchCriteria.validateStartDate(exceededDayInStartDate));
+        String exceededDayInStartDate = "2016-12-56";
+        assertEquals(false, searchCriteriaValidator.validateStartDate(exceededDayInStartDate));
     }
 
     @Test
     public void exceeded_month_in_startdate_should_return_false() {
-        String exceededMonthInStartDate = "12/56/2016";
-        assertEquals(false, searchCriteria.validateStartDate(exceededMonthInStartDate));
+        String exceededMonthInStartDate = "2016-56-12";
+        assertEquals(false, searchCriteriaValidator.validateStartDate(exceededMonthInStartDate));
     }
 
     @Test
     public void exceeded_year_in_startdate_should_return_false() {
-        String exceededYearInStartDate = "16/02/5698";
-        assertEquals(false, searchCriteria.validateStartDate(exceededYearInStartDate));
+        String exceededYearInStartDate = "5698-02-16";
+        assertEquals(false, searchCriteriaValidator.validateStartDate(exceededYearInStartDate));
     }
 
     @Test
     public void too_long_day_in_startdate_should_return_false() {
-        String tooLongDayInStartDate = "123/12/2016";
-        assertEquals(false, searchCriteria.validateStartDate(tooLongDayInStartDate));
+        String tooLongDayInStartDate = "2016-12-123";
+        assertEquals(false, searchCriteriaValidator.validateStartDate(tooLongDayInStartDate));
     }
 
     @Test
     public void too_long_month_in_startdate_should_return_false() {
-        String tooLongMonthInStartDate = "12/123/2016";
-        assertEquals(false, searchCriteria.validateStartDate(tooLongMonthInStartDate));
+        String tooLongMonthInStartDate = "2016-123-12";
+        assertEquals(false, searchCriteriaValidator.validateStartDate(tooLongMonthInStartDate));
     }
 
     @Test
     public void too_long_year_in_startdate_should_return_false() {
-        String tooLongYearInStartDate = "16/02/569825";
-        assertEquals(false, searchCriteria.validateStartDate(tooLongYearInStartDate));
+        String tooLongYearInStartDate = "569825-02-16";
+        assertEquals(false, searchCriteriaValidator.validateStartDate(tooLongYearInStartDate));
     }
 
     @Test
     public void startdate_in_unacceptable_format_should_return_false() {
-        String startDateInUnacceptableFormat = "16-02-2016";
-        assertEquals(false, searchCriteria.validateStartDate(startDateInUnacceptableFormat));
+        String startDateInUnacceptableFormat = "2016/02/16";
+        assertEquals(false, searchCriteriaValidator.validateStartDate(startDateInUnacceptableFormat));
     }
 
 
     @Test
     public void exceeded_day_in_enddate_should_return_false() {
-        String exceededDayInEndDate = "56/12/2016";
-        assertEquals(false, searchCriteria.validateStartDate(exceededDayInEndDate));
+        String exceededDayInEndDate = "2016-12-56";
+        assertEquals(false, searchCriteriaValidator.validateStartDate(exceededDayInEndDate));
     }
 
     @Test
     public void exceeded_month_in_enddate_should_return_false() {
-        String exceededMonthInEndDate = "12/56/2016";
-        assertEquals(false, searchCriteria.validateStartDate(exceededMonthInEndDate));
+        String exceededMonthInEndDate = "2016-56-02";
+        assertEquals(false, searchCriteriaValidator.validateStartDate(exceededMonthInEndDate));
     }
 
     @Test
     public void exceeded_year_in_enddate_should_return_false() {
-        String exceededYearInEndDate = "16/02/5698";
-        assertEquals(false, searchCriteria.validateStartDate(exceededYearInEndDate));
+        String exceededYearInEndDate = "5698-02-16";
+        assertEquals(false, searchCriteriaValidator.validateStartDate(exceededYearInEndDate));
     }
 
     @Test
     public void tooLongDayInEndDateReturnsFalse() {
-        String tooLongDayInEndDate = "123/12/2016";
-        assertEquals(false, searchCriteria.validateStartDate(tooLongDayInEndDate));
+        String tooLongDayInEndDate = "2016-12-123";
+        assertEquals(false, searchCriteriaValidator.validateStartDate(tooLongDayInEndDate));
     }
 
     @Test
     public void too_long_month_in_enddate_should_return_false() {
-        String tooLongMonthInEndDate = "12/123/2016";
-        assertEquals(false, searchCriteria.validateStartDate(tooLongMonthInEndDate));
+        String tooLongMonthInEndDate = "2016-123-12";
+        assertEquals(false, searchCriteriaValidator.validateStartDate(tooLongMonthInEndDate));
     }
 
     @Test
     public void too_long_year_in_enddate_should_return_false() {
-        String tooLongYearInEndDate = "16/02/569825";
-        assertEquals(false, searchCriteria.validateStartDate(tooLongYearInEndDate));
+        String tooLongYearInEndDate = "569825-02-16";
+        assertEquals(false, searchCriteriaValidator.validateStartDate(tooLongYearInEndDate));
     }
 
     @Test
     public void enddate_in_unacceptable_format_should_return_false() {
         String endDateInUnacceptableFormat = "16-02-2016";
-        assertEquals(false, searchCriteria.validateStartDate(endDateInUnacceptableFormat));
+        assertEquals(false, searchCriteriaValidator.validateStartDate(endDateInUnacceptableFormat));
     }
-
-
-    @Test
-    public void day_in_startdate_after_day_in_enddate_should_return_false() {
-        String startDayAfterEndDay_StartDate = "16/10/2016";
-        String startDayAfterEndDay_EndDate = "10/10/2016";
-        assertEquals(searchCriteria.validateStartDate(startDayAfterEndDay_EndDate),
-                searchCriteria.validateStartDate(startDayAfterEndDay_StartDate));
-    }
-
-    @Test
-    public void month_in_startdate_after_month_in_enddate_should_return_false() {
-        String startMonthAfterEndMonth_StartDate = "03/10/2016";
-        String startMonthAfterEndMonth_EndDate = "03/03/2016";
-
-        assertEquals(searchCriteria.validateStartDate(startMonthAfterEndMonth_StartDate),
-                searchCriteria.validateStartDate(startMonthAfterEndMonth_EndDate));
-    }
-
-    @Test
-    public void year_in_startdate_after_year_in_enddate_should_return_false() {
-        String startYearAfterEndYear_StartDate = "03/06/2017";
-        String startYearAfterEndYear_EndDate = "03/06/2014";
-
-        assertEquals(searchCriteria.validateStartDate(startYearAfterEndYear_StartDate),
-                searchCriteria.validateStartDate(startYearAfterEndYear_EndDate));
-    }
-
 }
