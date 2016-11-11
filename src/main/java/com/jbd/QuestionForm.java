6fcfae6fc2461b37db554f07d645b5fa179901a9
Karolina Kuczyńska <kaarolcia.k@gmail.com>
2016-11-11 22:02:09 +0100

    now user will be asked for additional email addresses if wants to give more than one

--- src/main/java/com/jbd/QuestionForm.java
+++ src/main/java/com/jbd/QuestionForm.java
@@ -4,15 +4,16 @@ import static com.jbd.SearchCriteria.*;
 
 public class QuestionForm {
 
-    private static String EMAIL_QUESTION = "Provide email address (xxxx@xx.xx)";
+    private static String EMAIL_QUESTION = "Provide email address.";
     private static String KEYWORD_QUESTION = "Provide keywords to find in email subject. Separate them with commas.";
     private static String STARTDATE_QUESTION = "Provide start date (YYYY-MM-DD)";
     private static String ENDDATE_QUESTION = "Provide end date (YYYY-MM-DD)";
     private static String NUMBER_OF_EMAILS_QUESTION = "How many email addresses would you like to find?";
 
-    private static String EMAIL_QUESTION_WRONG_VALUE = "Wrong format, please try again (xxxx@xx.xx)";
+    private static String EMAIL_QUESTION_WRONG_VALUE = "Wrong format, please try again.";
     private static String STARTDATE_QUESTION_WRONG_VALUE = "Wrong format or invalid date, please provide new start date (YYYY-MM-DD)";
     private static String ENDDATE_QUESTION_WRONG_VALUE = "Wrong format or invalid date, please provide new end date (YYYY-MM-DD)";
+    private static String ADDITIONAL_EMAIL_QUESTION = "Provide additional email address.";
 
     public void askQuestionsAndSetAnswers() {
         UserCommunication sendMessage = new UserCommunication();
@@ -22,13 +23,17 @@ public class QuestionForm {
         int numberOfEmailAddresses;
         sendMessage.sendUserMessage(NUMBER_OF_EMAILS_QUESTION);
         numberOfEmailAddresses = Integer.valueOf(sendMessage.getUserResponse());
+        System.out.println(numberOfEmailAddresses);
 
         for(int index = 0; index < numberOfEmailAddresses; index++) {
             do {
                 sendMessage.sendUserMessage(EMAIL_QUESTION);
                 setEMAIL(sendMessage.getUserResponse());
+                System.out.println("Email: " + getEMAIL());
                 if (searchCriteriaValidator.validateEmail(searchCriteria.getEMAIL()) == false) {
                     EMAIL_QUESTION = EMAIL_QUESTION_WRONG_VALUE;
+                } else if(!(numberOfEmailAddresses==1 && index==0)) {
+                    EMAIL_QUESTION = ADDITIONAL_EMAIL_QUESTION;
                 }
             } while (!searchCriteriaValidator.validateEmail(searchCriteria.getEMAIL()));
         }
