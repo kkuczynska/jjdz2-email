a1207dadddefaa6b22f238dc03643918913a3180
Karolina Kuczyńska <kaarolcia.k@gmail.com>
2016-11-14 23:09:49 +0100

    refactor

--- src/main/java/com/jbd/QuestionForm.java
+++ src/main/java/com/jbd/QuestionForm.java
@@ -26,34 +26,34 @@ public class QuestionForm {
         numberOfEmailAddresses = Integer.valueOf(sendMessage.getUserResponse());
         System.out.println(numberOfEmailAddresses);
 
-        for(int index = 0; index < numberOfEmailAddresses; index++) {
+        for (int index = 0; index < numberOfEmailAddresses; index++) {
             do {
                 sendMessage.sendUserMessage(EMAIL_QUESTION);
                 emailAddress = sendMessage.getUserResponse();
                 setEMAIL(emailAddress);
                 if (searchCriteriaValidator.validateEmail(emailAddress) == false) {
                     EMAIL_QUESTION = EMAIL_QUESTION_WRONG_VALUE;
-                } else if(!(numberOfEmailAddresses==1 && index==0)) {
+                } else if (!(numberOfEmailAddresses == 1 && index == 0)) {
                     EMAIL_QUESTION = ADDITIONAL_EMAIL_QUESTION;
                 }
             } while (!searchCriteriaValidator.validateEmail(emailAddress));
         }
 
         do {
-        sendMessage.sendUserMessage(STARTDATE_QUESTION);
-        setSTARTDATE(sendMessage.getUserResponse());
-            if(searchCriteriaValidator.validateStartDate(searchCriteria.getSTARTDATE())==false) {
-                STARTDATE_QUESTION=STARTDATE_QUESTION_WRONG_VALUE;
+            sendMessage.sendUserMessage(STARTDATE_QUESTION);
+            setSTARTDATE(sendMessage.getUserResponse());
+            if (searchCriteriaValidator.validateStartDate(searchCriteria.getSTARTDATE()) == false) {
+                STARTDATE_QUESTION = STARTDATE_QUESTION_WRONG_VALUE;
             }
-        } while(!searchCriteriaValidator.validateStartDate(searchCriteria.getSTARTDATE()));
+        } while (!searchCriteriaValidator.validateStartDate(searchCriteria.getSTARTDATE()));
 
         do {
-        sendMessage.sendUserMessage(ENDDATE_QUESTION);
-        setENDDATE(sendMessage.getUserResponse());
-            if(searchCriteriaValidator.validateEndDate(searchCriteria.getENDDATE())==false) {
-                ENDDATE_QUESTION=ENDDATE_QUESTION_WRONG_VALUE;
+            sendMessage.sendUserMessage(ENDDATE_QUESTION);
+            setENDDATE(sendMessage.getUserResponse());
+            if (searchCriteriaValidator.validateEndDate(searchCriteria.getENDDATE()) == false) {
+                ENDDATE_QUESTION = ENDDATE_QUESTION_WRONG_VALUE;
             }
-        } while(!searchCriteriaValidator.validateEndDate(searchCriteria.getENDDATE()));
+        } while (!searchCriteriaValidator.validateEndDate(searchCriteria.getENDDATE()));
 
         sendMessage.sendUserMessage(KEYWORD_QUESTION);
         setKEYWORDS(sendMessage.getUserResponse());
