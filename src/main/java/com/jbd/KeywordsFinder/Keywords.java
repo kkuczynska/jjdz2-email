48105d584c5a270eff606907c052c34601ad1682
Karolina Kuczyńska <kaarolcia.k@gmail.com>
2016-11-16 19:40:43 +0100

    added method displaying results, whole form is called in one method

---
+++ src/main/java/com/jbd/KeywordsFinder/Keywords.java
@@ -0,0 +1,47 @@
+package com.jbd.KeywordsFinder;
+
+import com.jbd.UserCommunication;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+import static com.jbd.QuestionForm.ANSWER_POSITIVE;
+
+public class Keywords {
+
+    private static Set<String> KEYWORDS_SET = new HashSet();
+    private static ArrayList<String> answersIDs = new ArrayList<>();
+    private static UserCommunication userCommunication = new UserCommunication();
+    private static final String NO_MATCHES = "Sorry! \nThere is no keywords matching your query.";
+
+    public void createAndDisplayKeywordsHashSet() {
+        createKeywordsSet();
+        displayKeywords();
+    }
+
+    public void gatherAnswers(String answerID) {
+        answersIDs.add(answerID);
+    }
+
+    private static void createKeywordsSet() {
+        JsonReader jsonReader = new JsonReader();
+        for (int index = 0; index < answersIDs.size(); index++) {
+            System.out.println("element: " + answersIDs.get(index));
+            if (answersIDs.get(index).equalsIgnoreCase(ANSWER_POSITIVE)) {
+                KEYWORDS_SET.addAll(jsonReader.readAnswerJsonArray(answersIDs.get(index)));
+            }
+        }
+        if (KEYWORDS_SET.isEmpty()) {
+            userCommunication.sendUserMessage(NO_MATCHES);
+        }
+    }
+
+    private static void displayKeywords() {
+        UserCommunication userCommunication = new UserCommunication();
+        for (String keyword : KEYWORDS_SET) {
+            userCommunication.sendUserMessage(keyword);
+        }
+    }
+
+}
