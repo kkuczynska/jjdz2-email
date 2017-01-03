69491c24ae0d012f3be9be98969b14f7495200f0
Karolina Kuczyńska <kaarolcia.k@gmail.com>
2017-01-03 22:29:41 +0100

    delete js file

--- src/main/webapp/resources/js.js
+++
@@ -1,5 +0,0 @@
-function validateFilePath() {
-    //if (document.getElementById("email").value == "") {
-        document.getElementById("emailPathMsg").innerHTML = "This field cannot be empty.";
-  //  }
-}
