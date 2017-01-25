1158dc51f2c5673f75bf60ddd49d0ee5b9fd7b0f
Karolina Kuczyńska <kaarolcia.k@gmail.com>
2017-01-25 21:41:20 +0100

    now number of unread emails are read after each click on body; deleted comments and usused functions

--- src/main/webapp/count.js
+++ src/main/webapp/count.js
@@ -4,7 +4,6 @@ var CLIENT_ID = '569832515580-0fdmssh0p1n5dafr11apkvf2mh61gero.apps.googleuserco
 
 var SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
 
-//Check if current user has authorized this application.
 function checkAuth() {
     gapi.auth.authorize(
         {
@@ -14,28 +13,16 @@ function checkAuth() {
         }, handleAuthResult);
 }
 
-/**
- * Handle response from authorization server.
- * @param {Object} authResult Authorization result.
- */
 function handleAuthResult(authResult) {
     var authorizeDiv = document.getElementById('authorize-div');
     if (authResult && !authResult.error) {
-        // Hide auth UI, then load client library.
         authorizeDiv.style.display = 'none';
         loadGmailApi();
     } else {
-        // Show auth UI, allowing the user to initiate authorization by
-        // clicking authorize button.
         authorizeDiv.style.display = 'inline';
     }
 }
 
-/**
- * Initiate auth flow in response to user clicking authorize button.
- *
- * @param {Event} event Button click event.
- */
 function handleAuthClick(event) {
     gapi.auth.authorize(
         {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
@@ -43,37 +30,8 @@ function handleAuthClick(event) {
     return false;
 }
 
-/**
- * Load Gmail API client library. List labels once client library
- * is loaded.
- */
 function loadGmailApi() {
-   gapi.client.load('gmail', 'v1', listLabels);
-    gapi.client.load('gmail', 'v1', countMessages);
-}
-
-/**
- * Print all Labels in the authorized user's inbox. If no labels
- * are found an appropriate message is printed.
- */
-function listLabels() {
-    var request = gapi.client.gmail.users.labels.list({
-        'userId': 'me'
-    });
-
-    request.execute(function (resp) {
-        var labels = resp.labels;
-        appendPre('Labels:');
-
-        if (labels && labels.length > 0) {
-            for (i = 0; i < labels.length; i++) {
-                var label = labels[i];
-                appendPre(label.name)
-            }
-        } else {
-            appendPre('No Labels found.');
-        }
-    });
+   gapi.client.load('gmail', 'v1', countMessages);
 }
 
 function countMessages() {
@@ -84,53 +42,22 @@ function countMessages() {
 
     request.execute(function (resp) {
         var messagesUnread = resp.messagesUnread;
-
-        // document.getElementById("count").innerHTML = messagesUnread;
         displayCount(messagesUnread);
     });
 }
 
-/*List all messages in mailbox */
-function listMessages() {
-    var userId = "me";
-    var request = gapi.client.gmail.users.messages.list({
-        'userId': userId
-    });
-
-    request.execute(function (resp) {
-        var messages = resp.messages;
-        var output = "<br>Query returned " + messages.length + " messages:<br>";
-        for (var i = 0; i < messages.length; i++) {
-            output += messages[i].id + "<br>";
-        }
-        document.getElementById("content").innerHTML += output;
-    });
-}
-
-/**
- * Append a pre element to the body containing the given message
- * as its text node.
- *
- * @param {string} message Text to be placed in pre element.
- */
 function appendPre(message) {
     var pre = document.getElementById('output');
     var textContent = document.createTextNode(message + '\n');
     pre.appendChild(textContent);
 }
 
-
-
-function displayCount(message) {
-    var pre = document.getElementById('count');
-    var textContent = document.createTextNode(message + '\n');
-    pre.appendChild(textContent);
+function displayCount(messagesUnread) {
+    document.getElementById('count').innerHTML = messagesUnread;
 }
 
-
 $(document).ready(function(){
-  //  handleAuthClick(event);
-    listMessages();
-    countMessages();
-
+    $(".body").click(function(){
+        countMessages();
+    });
 });
