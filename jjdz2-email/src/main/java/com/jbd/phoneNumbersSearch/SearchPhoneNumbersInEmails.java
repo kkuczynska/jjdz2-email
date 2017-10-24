d7d7bdba35a6289fba3166e72b3e77a1d0f15349
Karolina Kuczyńska <kaarolcia.k@gmail.com>
2017-10-24 23:06:46 +0200

    refactor: split long method into few smaller, shorten other methods too

--- jjdz2-email/src/main/java/com/jbd/phoneNumbersSearch/SearchPhoneNumbersInEmails.java
+++ jjdz2-email/src/main/java/com/jbd/phoneNumbersSearch/SearchPhoneNumbersInEmails.java
@@ -20,52 +20,50 @@ public class SearchPhoneNumbersInEmails {
     private static final Marker MARKER = MarkerFactory.getMarker("SearchPhoneNumbersInEmails");
 
     private String phonePattern = "(\\s\\d{3}[\\-,\\s]?\\d{3}[\\-,\\s]?\\d{3}\\b)"; // 515417888
+    private Map<String, List<String>> phoneNumbersMap = new HashMap<>();
 
     public Map<String, List<String>> searchPhoneNumbers(List<Email> emailList) {
-        Map<String, List<String>> resultMap = new HashMap<>();
         Pattern patternPhone = Pattern.compile(phonePattern);
-        boolean isDuplicate = false;
-        for (Email email : emailList) {
-            Matcher matcher = patternPhone.matcher(email.getContent());
-            while (matcher.find()) {
-                String phoneNumber = replaceWrongCharactersInNumber(matcher.group());
-                if (resultMap.containsKey(email.getFrom())) {
-                    LOGGER.info(MARKER, "Found User: " + email.getFrom());
-                    isDuplicate = searchDuplicates(resultMap, phoneNumber);
-                    if (!isDuplicate) {
-                        LOGGER.info(MARKER, "No duplicates found, adding number: " + phoneNumber + " for: " + email.getFrom());
-                        List<String> phoneNumberList;
-                        phoneNumberList = resultMap.get(email.getFrom());
-                        phoneNumberList.add(phoneNumber);
-                        resultMap.put(email.getFrom(), phoneNumberList);
-                    } else {
-                        LOGGER.info(MARKER, "Phone number: " + phoneNumber + " already exists, no add");
-                    }
-                } else {
-                    LOGGER.info(MARKER, "New user: " + email.getFrom());
-                    List<String> phoneList = new ArrayList<>();
-                    phoneList.add(phoneNumber);
-                    LOGGER.info(MARKER, "Added number: " + phoneNumber + " for user: " + email.getFrom());
-                    resultMap.put(email.getFrom(), phoneList);
-                }
-            }
-        }
-        return resultMap;
+        emailList.forEach(email -> collectPhoneNumbers(phoneNumbersMap, email, patternPhone.matcher(email.getContent())));
+        return phoneNumbersMap;
     }
 
-    private boolean searchDuplicates(Map<String, List<String>> mapToSearch, String stringTooSearch) {
-        boolean foundDuplicate = false;
-        for (String key : mapToSearch.keySet()) {
-            for (String value : mapToSearch.get(key)) {
-                if (value.trim().equals(stringTooSearch)) {
-                    foundDuplicate = true;
-                    break;
-                }
-            }
-            if (foundDuplicate)
-                break;
+    private void collectPhoneNumbers(Map<String, List<String>> resultMap, Email email, Matcher matcher) {
+        while (matcher.find()) {
+            String phoneNumber = replaceWrongCharactersInNumber(matcher.group());
+            collectNumbersToMap(resultMap, email, phoneNumber);
         }
-        return foundDuplicate;
+    }
+
+    private void collectNumbersToMap(Map<String, List<String>> resultMap, Email email, String phoneNumber) {
+        if (resultMap.containsKey(email.getFrom())) {
+            if (!findDuplicatedNumbers(resultMap, phoneNumber)) {
+                LOGGER.info(MARKER, "No duplicates found, adding number: " + phoneNumber + " for: " + email.getFrom());
+                putElementToMap(resultMap, email, phoneNumber, resultMap.get(email.getFrom()));
+            } else {
+                LOGGER.info(MARKER, "Phone number: " + phoneNumber + " already exists");
+            }
+        } else {
+            LOGGER.info(MARKER, "Added number: " + phoneNumber + " for user: " + email.getFrom());
+            putElementToMap(resultMap, email, phoneNumber, new ArrayList<>());
+        }
+    }
+
+    private void putElementToMap(Map<String, List<String>> resultMap, Email email, String phoneNumber, List<String> phoneNumberList) {
+        phoneNumberList.add(phoneNumber);
+        resultMap.put(email.getFrom(), phoneNumberList);
+    }
+
+    private boolean findDuplicatedNumbers(Map<String, List<String>> mapToSearch, String stringToSearch) {
+        final boolean[] foundDuplicate = {false};
+        mapToSearch.keySet().forEach(key -> {
+            mapToSearch.get(key).forEach(value -> {
+                if (value.equals(stringToSearch)) {
+                    foundDuplicate[0] = true;
+                }
+            });
+        });
+        return foundDuplicate[0];
     }
 
     private String replaceWrongCharactersInNumber(String phoneNumber) {
